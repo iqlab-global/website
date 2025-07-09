7872e925d8a435918915deb37625bedcb4f39990
Cristian Ramirez <ramireze4@gmail.com>
2025-07-09 08:28:18 -0600

    IW-I23 testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,10 +55,7 @@ export default async function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <meta
-          name='viewport'
-          content='width=device-width,initial-scale=1,viewport-fit=cover'
-        />
+        <meta name='viewport' content='width=device-width,initial-scale=1' />
       </head>
 
       <body className={clsx(disketMono.variable, bdo.variable)}>
