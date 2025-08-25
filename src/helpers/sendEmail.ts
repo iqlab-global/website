802a9ac140d96c0f1d7e0c4e4a6cdfdc61e5c16f
Cristian Ramirez <ramireze4@gmail.com>
2025-08-25 12:44:17 -0600

    IWI19 tweak setup

--- src/helpers/sendEmail.ts
+++ src/helpers/sendEmail.ts
@@ -4,11 +4,12 @@ import nodemailer from 'nodemailer';
 const SMTP_SERVER_HOST = process.env.SMTP_SERVER_HOST;
 const SMTP_SERVER_USERNAME = process.env.SMTP_SERVER_USERNAME;
 const SMTP_SERVER_PASSWORD = process.env.SMTP_SERVER_PASSWORD;
+const SMTP_SERVER_PORT = parseInt(process.env.SMTP_SERVER_PORT!);
 
 const transporter = nodemailer.createTransport({
   host: SMTP_SERVER_HOST,
-  port: 587,
-  secure: false,
+  port: SMTP_SERVER_PORT,
+  secure: true,
   auth: {
     user: SMTP_SERVER_USERNAME,
     pass: SMTP_SERVER_PASSWORD,
